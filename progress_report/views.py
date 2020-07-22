532d8bca4c6824e5452d307312e5d100be4e2cdf
Fahim Faiaz <fahim7faiaz@gmail.com>
2020-07-22 11:56:39 +0600

    show the access records.

--- progress_report/views.py
+++ progress_report/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from progress_report.models import AccessRecord, Webpage, Topic
 
 
 # Create your views here.
 def index(request):
-    my_dict = {'insert_me': 'yoyo bro!'}
-    return render(request, 'progress_report/index.html', context=my_dict)
+    webpages_list = AccessRecord.objects.order_by('date')
+    date_dict = {'access_records': webpages_list}
+    return render(request, 'progress_report/index2.html', context=date_dict)
